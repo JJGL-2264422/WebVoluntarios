b73a7137e8f1235dd440e4324c97501836260c81
JJGL-2264422 <jjgl14ag03@gmail.com>
2024-09-08 13:43:45 -0500

    Esqueleto del login, css del bootstrap y CSVs de la base de datos

---
+++ login.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title>Iniciar Sesión</title>
+        <link rel="stylesheet" href="./css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4>Iniciar Sesión</h4>
+                        </div>
+                        <div class="card-body">
+                            <form>
+                            <div class = "form-group">
+                            <label for="exampleInputEmail1">Correo Electronico</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"> <br>
+                            </div>
+                            <div class="form-group">
+                            <label for="exampleInputPassword1">Contraseña</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"> <br>
+                            </div>
+                            <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Sign In</button>
+                            </form>
+                            
+                            
+                        </div>
+                        <div class="card-footer text-muted"></div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </body>
+</html>
+
