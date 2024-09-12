dbd6beddd009eb8559e67e52f4339692c2aa5f79
Crisdamapa2210 <danimartinez.cdmp@gmail.com>
2024-09-12 15:00:24 -0500

    progeso de menu principal

--- vista/MenuPrincipal.php
+++ vista/MenuPrincipal.php
@@ -11,22 +11,22 @@
 <body>
  <?php 
     incluir_navbar();
- ?>
+ ?><br>
     <div class="container text-center">
         <div class="row ">
-            <div class="col">
+            <div class="col"><br><br><br>
                 <div id="carouselExample" class="carousel slide">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <img src="../imagen/voluntario3.png" class="d-block w-100" alt="...">
+                        <img src="../imagen/manos-voluntariado.jpg" class="d-block w-100" alt="...">
                         </div>
 
                         <div class="carousel-item">
-                        <img src="../imagen/voluntarios.png" class="d-block w-100" alt="...">
+                        <img src="../imagen/remake.png" class="d-block w-100" alt="...">
                         </div>
 
                         <div class="carousel-item">
-                        <img src="../imagen/voluntario2.png" class="d-block w-100" alt="...">
+                        <img src="../imagen/images.png" class="d-block w-100" alt="...">
                         </div>
 
                     </div>
@@ -44,20 +44,32 @@
 
             </div>
             <div class="col">
-                <h1>ya no she</h1>
+                <div>
+                    <h1>Voluntariados S.A</h1>
+                </div>
+                <div>
+                Bienvenidos a Voluntariados S.A <br>
+
+                    En Voluntarios S.A, creemos que el poder del cambio está en las manos de cada uno de nosotros. Somos una plataforma dedicada a conectar a personas apasionadas con oportunidades de voluntariado que marcan la diferencia. Ya sea que busques contribuir con tu tiempo, habilidades o energía, aquí encontrarás una gran variedad de proyectos que se alinean con tus intereses y valores. <br><br>
+
+                    El voluntariado es más que una acción desinteresada; es una oportunidad para aprender, crecer y compartir. En un mundo donde cada gesto cuenta, tu participación tiene el potencial de transformar comunidades, proteger el medio ambiente, apoyar causas humanitarias o mejorar la calidad de vida de los más vulnerables. Sabemos que cada persona tiene algo único que ofrecer, y es por eso que trabajamos para brindarte opciones accesibles y significativas, tanto a nivel local como internacional. <br><br>
+
+                    No importa si tienes horas libres cada semana o solo algunos días al año, siempre hay una oportunidad de hacer algo positivo. Navega por nuestras categorías de voluntariado y descubre cómo puedes convertirte en parte del cambio. Desde programas de conservación, enseñanza, asistencia médica, hasta el apoyo en desastres naturales, cada proyecto tiene un impacto profundo. <br><br>
+
+                    ¡Únete a nuestra comunidad de voluntarios hoy mismo! Juntos, podemos construir un futuro más solidario, equitativo y sostenible. <br><br>
+                </div>
             </div>
         </div>
     </div>
-
+<br><br><br><br><br><br><br><br><br><br><br><br>
     <footer>
         <div class="bg-secondary">
             <div class="container text-center ">
                 <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-                    <div class="col">Desarrolladores</div>
-                    <div class="col">¿Quienes somos?</div>
-                    <div class="col">Contactanos</div>
-                    <div class="col">Ubicanos</div>
-                    
+                    <div class="col"><h5>Desarrolladores</h5> <br>Cristian Martines <br>Juan Jose Garcia <br>Christian Trujillo </div>
+                    <div class="col"><h5>¿Quienes somos?</h5><br> dirigete a nuestra seccion <br> de "¿Quienes somos?"</div>
+                    <div class="col"><h5>Contactanos</h5><br> ° 3188943014 <br>°cristian.daniel.martinez@correounivalle.edu.co <br>° 2397362</div>
+                    <div class="col"><h5>Ubicanos</h5><br>° carrea 13 #5-25</div>                    
                 </div>
             </div>  
         </div>
--- vista/navbar_vista.php
+++ vista/navbar_vista.php
@@ -22,7 +22,7 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="../Vista/signup_ususario.php">Registrar</a>
+          <a class="nav-link" href="formularios/registro.php">Registrar</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
