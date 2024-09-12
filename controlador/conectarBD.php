b70b6b8c34fa7b17c2c2c628fde88c6302791ea4
JJGL-2264422 <jjgl14ag03@gmail.com>
2024-09-12 11:01:51 -0500

    Metodo equivocado solucionado.

--- controlador/conectarBD.php
+++ controlador/conectarBD.php
@@ -1,18 +1,13 @@
 <?php
-    class CConexion{
-        function ConexionBD(){
-            $host = "localhost";
-            $dbname = "bdwebvol";
-            $user = "postgres";
-            $pass = "postgres";
+    $host = "localhost";
+    $dbname = "bdwebvol";
+    $user = "postgres";
+    $pass = "postgres";
             
-            try{
-                $conn = new PDO ("pgsql:host=$host; dbname=$dbname", $user, $pass);
-                if($conn){echo "Conectado!";}
-            }
-            catch(PDOException $exp){
-                echo ("Error al conectarse a la base de datos: $exp");
-            }
-        }
+    try{
+        $conn = new PDO ("pgsql:host=$host;dbname=$dbname", $user, $pass);
+    }
+    catch(PDOException $exp){
+        echo ("Error al conectarse a la base de datos: $exp");
     }
 ?>
