dc7529c58fd7b181d202a536ee5b99a1b162a9ba
JJGL-2264422 <jjgl14ag03@gmail.com>
2024-09-16 17:09:53 -0500

    Esqueleto de perfil.php

--- vista/perfil.php
+++ vista/perfil.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>NOMBRE DEL USUARIO</title></head>
   <body>
         <nav class="navbar navbar-expand navbar-light bg-light">
@@ -12,63 +13,76 @@
                 <a class="nav-item nav-link" href="#">Home</a>
             </div>
         </nav>
-        
-        <div class="my-md-5 mx-auto" style="max-width: 1100px;">
-          <div class="row no-gutters" style="margin:5px;">
 
-            <div class="col-lg-4" style="padding: 3px">
-              <div class="card bg-faded tab-content">
-                <div class="card border-0 bg-faded" style="height: 250px;
-                background-image:url(https://imgur.com/hBcLTUo.png);
-                background-size:contain;
-                background-position:center;
-                background-repeat:no-repeat;
-                "></div>
-                <div class="my-2 lead display-6 text-uppercase text-center" style="height: 272px;">
-                  $username
+        <div class="container" style="margin-top:2%">
+          <div class="main">
+            <div class="row">
+
+            <!--Card de la imagen y nombre de usuario-->
+              <div class="col-md-4 mt-1">
+                <div class="card text-center sidebar">
+                  <div class="col-12 card bg-faded mt-2" style="overflow:hidden;">
+                    <div style="height:125px; background-color:#5c5b5b"></div>
+                    <a class="bg-faded p-1 rounded-circle ml-3" style="margin-top: -70px">
+                      <img src="https://imgur.com/hBcLTUo.png" class="rounded-circle" style="border: 7px solid #303030; background-color:#303030" width="150">
+                    </a>
+                    <div class="mt-3">
+                      <h3>$username</h3>
+                      <p><span class="fa fa-star"></span>
+                      <span class="fa fa-star"></span>
+                      <span class="fa fa-star"></span>
+                      <span class="fa fa-star-o"></span>
+                      <span class="fa fa-star-o"></span></p>
+                      <p>$valoración</p>
+                      <!--Espacio para más datos-->
+                    </div>
+                  </div>
                 </div>
               </div>
 
-            </div>
+              <div class="col-md-8 mt-1">
 
-            <div class="col-lg-7 order-md-1 order-1" style="padding: 3px;">
-              <div class="card bg-faded tab-content" style="height: 540px;">
-                  <div class="card-body">
-                    <h3 style="font-weight:bold;">Detalles personales</h3><hr>
-
-                    <div class="row no gutters my-n2">
-
-                      <div class="col-sm-6-my-1">
-                      <span class="mr-2" style="font-weight: bold; color:steelblue;">Nombre completo: </span>
-                      $nombre $apellido
-                      </div>
-
-                      <div class="col-sm-6-my-1">
-                      <span class="mr-2" style="font-weight: bold; color:steelblue;">Correo electronico: </span>
-                      $email
-                      </div>
-
-                      <div class="col-sm-6-my-1">
-                      <span class="mr-2" style="font-weight: bold; color:steelblue;">Edad: </span>
-                      $edad
-                      </div>
-
-                      <div class="col-sm-6-my-1">
-                      <span class="mr-2" style="font-weight: bold; color:steelblue;">Trabaja para: </span>
-                      $comp
-                      </div>
-
-                      <div class="col-sm-6-my-1">
-                      <span class="mr-2" style="font-weight: bold; color:steelblue;">Valoración: </span>
-                      $valor
-                      </div>
-
+                <div class="card mb-3 content">
+                  <h1 class="m-3 pt-3">Datos personales</h1>
+                  <!--Filas de info-->
+                  <div class="card-body"> <!--Un row por cada campo de información-->
+                    <div class="row"> 
+                        <div class="col-md-3">
+                          <h5>Nombre completo</h5>
+                        </div>
+                        <div class="col-md-9 text-light">
+                          $nombre $apellido
+                        </div>
                     </div><hr>
-                    <div>W.I.P</div>
+                    <div class="row"> 
+                        <div class="col-md-3">
+                          <h5>Correo Electronico</h5>
+                        </div>
+                        <div class="col-md-9 text-light">
+                          $correo
+                        </div>
+                    </div><hr>
                   </div>
-              </div>
-            </div>
+                </div>
 
+                <div class="card mb-3 content">
+                  <h1 class="m-3 pt-3">Registros</h1>
+                  <!--Filas de info-->
+                  <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-3">
+                          <h5>-Proximamente-</h5>
+                        </div>
+                        <div class="col-md-9 text-light">
+                          -
+                        </div>
+                    </div><hr>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
           </div>
         </div>
 
