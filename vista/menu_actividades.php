47619dfdc4af6cd899db5547117e6cd61cc5d2a4
JJGL-2264422 <jjgl14ag03@gmail.com>
2024-09-24 16:39:19 -0500

    Esqueleto de "menu_actividades.php" añadido.

---
+++ vista/menu_actividades.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Actividades</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand navbar-light bg-light justify-content-between">
+        <div class="nav navbar-nav">
+          <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-item nav-link" href="#">Actividades</a>
+        </div>
+        <div class="nav navbar-nav">
+          <a class="nav-item nav-link" href="#">Cerrar sesión</a>
+        </div>
+    </nav>
+
+    <div class="clearfix container-fluid main-container full-sidebar">
+        <div class="row row-offcanvas row-offcanvas-left" style="min-height: 537px;">
+            <div class="col-md-3 col-lg-2 sidebar sidebar-offcanvas">
+
+            </div>
+            <div class="col-sm-12 col-md-9 col-lg-10 content-main">
+                
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
