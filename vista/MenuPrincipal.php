eb84d0fd424dfc2793296c3afc1a8402d4e1c1b9
JJGL-2264422 <jjgl14ag03@gmail.com>
2024-09-25 16:54:05 -0500

    Cambios graficos menores.

--- vista/MenuPrincipal.php
+++ vista/MenuPrincipal.php
@@ -6,15 +6,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Voluntarios S.A</title>
 </head>
 <body>
  <?php 
     incluir_navbar();
  ?><br>
-    <div class="container text-center">
+    <div class="container text-center" style="margin-bottom:80px;">
         <div class="row ">
-            <div class="col"><br><br><br>
+            <div class="col" style="margin-top:115px;">
                 <div id="carouselExample" class="carousel slide">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
@@ -45,11 +47,9 @@
             </div>
             <div class="col">
                 <div>
-                    <h1>Voluntariados S.A</h1>
+                    <br><h1>Voluntariados S.A</h1><br>
                 </div>
                 <div>
-                Bienvenidos a Voluntariados S.A <br>
-
                     En Voluntarios S.A, creemos que el poder del cambio está en las manos de cada uno de nosotros. Somos una plataforma dedicada a conectar a personas apasionadas con oportunidades de voluntariado que marcan la diferencia. Ya sea que busques contribuir con tu tiempo, habilidades o energía, aquí encontrarás una gran variedad de proyectos que se alinean con tus intereses y valores. <br><br>
 
                     El voluntariado es más que una acción desinteresada; es una oportunidad para aprender, crecer y compartir. En un mundo donde cada gesto cuenta, tu participación tiene el potencial de transformar comunidades, proteger el medio ambiente, apoyar causas humanitarias o mejorar la calidad de vida de los más vulnerables. Sabemos que cada persona tiene algo único que ofrecer, y es por eso que trabajamos para brindarte opciones accesibles y significativas, tanto a nivel local como internacional. <br><br>
@@ -61,15 +61,13 @@
             </div>
         </div>
     </div>
-<br><br><br><br><br><br><br>
     <footer>
         <div class="bg-secondary">
-            <div class="container text-center ">
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-                    <div class="col"><h5>Desarrolladores</h5> <br>Cristian Martines <br>Juan Jose Garcia <br>Christian Trujillo </div>
-                    <div class="col"><h5>¿Quienes somos?</h5><br> dirigete a nuestra seccion <br> de "¿Quienes somos?"</div>
-                    <div class="col"><h5>Contactanos</h5><br> ° 3188943014 <br>°cristian.daniel.martinez@correounivalle.edu.co <br>° 2397362</div>
-                    <div class="col"><h5>Ubicanos</h5><br>° carrea 13 #5-25</div>                    
+            <div class="container text-center" style="padding-top:30px;padding-bottom:20px">
+                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 justify-content-center">
+                    <div class="col"><h5>Desarrolladores</h5><hr>Cristian Martinez <br>Juan Jose Garcia <br>Christian Trujillo </div>
+                    <div class="col"><h5>Contactanos</h5><hr>3188943014 <br>cristian.daniel.martinez@correounivalle.edu.co <br>° 2397362</div>
+                    <div class="col"><h5>Ubicanos</h5><hr>Carrera 13 #5-25</div>                    
                 </div>
             </div>  
         </div>
