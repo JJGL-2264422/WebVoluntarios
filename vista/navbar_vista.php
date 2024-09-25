98df44ec55bb6bae3b8d4e7ed63c136fe723e645
JJGL-2264422 <jjgl14ag03@gmail.com>
2024-09-25 16:54:40 -0500

    Script de bootstrap eliminado.

--- vista/navbar_vista.php
+++ vista/navbar_vista.php
@@ -4,8 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Otra Página</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-    rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body> 
 <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
@@ -17,10 +15,8 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-       
-
         <li class="nav-item">
-          <a class="nav-link" href="formularios/registro.php">Registrar</a>
+          <a class="nav-link" href="formularios/registro.php">Registrarse</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="formularios/login.php">Iniciar Sesión</a>
@@ -33,9 +29,5 @@
     </div>
   </div>
 </nav>
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
