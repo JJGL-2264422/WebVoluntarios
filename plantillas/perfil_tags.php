fc1d72f0a2c148a2a997432ee9de23de61c1df21
JJGL-2264422 <jjgl14ag03@gmail.com>
2024-10-03 14:18:26 -0500

    Mejoras visuales

--- plantillas/perfil_tags.php
+++ plantillas/perfil_tags.php
@@ -7,89 +7,97 @@
             </div>
             <form method="post">
                 <div class="modal-body">
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Cocina" id="etiquetaCocina">
-                        <label class="form-check-label" for="etiquetaCocina">Cocina</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Animales domésticos" id="etiquetaAnimalesDomesticos">
-                        <label class="form-check-label" for="etiquetaAnimalesDomesticos">Animales domésticos</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Animales de granja" id="etiquetaAnimalesGranja">
-                        <label class="form-check-label" for="etiquetaAnimalesGranja">Animales de granja</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Niños" id="etiquetaNinos">
-                        <label class="form-check-label" for="etiquetaNinos">Niños</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Atención a personas mayores" id="etiquetaAtencionMayores">
-                        <label class="form-check-label" for="etiquetaAtencionMayores">Atención a personas mayores</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Manualidades" id="etiquetaManualidades">
-                        <label class="form-check-label" for="etiquetaManualidades">Manualidades</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Deportes" id="etiquetaDeportes">
-                        <label class="form-check-label" for="etiquetaDeportes">Deportes</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Enseñanza" id="etiquetaEnsenanza">
-                        <label class="form-check-label" for="etiquetaEnsenanza">Enseñanza</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Tecnología" id="etiquetaTecnologia">
-                        <label class="form-check-label" for="etiquetaTecnologia">Tecnología</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Fotografía" id="etiquetaFotografia">
-                        <label class="form-check-label" for="etiquetaFotografia">Fotografía</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Jardinería" id="etiquetaJardineria">
-                        <label class="form-check-label" for="etiquetaJardineria">Jardinería</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Traducción" id="etiquetaTraduccion">
-                        <label class="form-check-label" for="etiquetaTraduccion">Traducción</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Gestión de redes sociales" id="etiquetaGestionRedes">
-                        <label class="form-check-label" for="etiquetaGestionRedes">Gestión de redes sociales</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Escritura" id="etiquetaEscritura">
-                        <label class="form-check-label" for="etiquetaEscritura">Escritura</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Informática" id="etiquetaInformatica">
-                        <label class="form-check-label" for="etiquetaInformatica">Informática</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Comunicación" id="etiquetaComunicacion">
-                        <label class="form-check-label" for="etiquetaComunicacion">Comunicación</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Contabilidad" id="etiquetaContabilidad">
-                        <label class="form-check-label" for="etiquetaContabilidad">Contabilidad</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Salud y bienestar" id="etiquetaSaludBienestar">
-                        <label class="form-check-label" for="etiquetaSaludBienestar">Salud y bienestar</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Investigación" id="etiquetaInvestigacion">
-                        <label class="form-check-label" for="etiquetaInvestigacion">Investigación</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Arte y creatividad" id="etiquetaArteCreatividad">
-                        <label class="form-check-label" for="etiquetaArteCreatividad">Arte y creatividad</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Entretenimiento" id="etiquetaEntretenimiento">
-                        <label class="form-check-label" for="etiquetaEntretenimiento">Entretenimiento</label>
+                    <div class="text-center" style="margin:-5px;padding-bottom:7px; padding-top:7px;border:3px solid #454545">
+                        <div class="form-check form-check-inline" style="margin-left:5px">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Cocina" id="etiquetaCocina">
+                            <label class="form-check-label" for="etiquetaCocina">Cocina</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Animales domésticos" id="etiquetaAnimalesDomesticos">
+                            <label class="form-check-label" for="etiquetaAnimalesDomesticos">Animales domésticos</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Animales de granja" id="etiquetaAnimalesGranja">
+                            <label class="form-check-label" for="etiquetaAnimalesGranja">Animales de granja</label>
+                        </div>
+                        <div style="margin-top:5px;margin-bottom:5px;border-top:3px solid #454545"></div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Niños" id="etiquetaNinos">
+                            <label class="form-check-label" for="etiquetaNinos">Niños</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Atención a personas mayores" id="etiquetaAtencionMayores">
+                            <label class="form-check-label" for="etiquetaAtencionMayores">Atención a personas mayores</label>
+                        </div>
+                        <div style="margin-top:5px;margin-bottom:5px;border-top:3px solid #454545"></div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Manualidades" id="etiquetaManualidades">
+                            <label class="form-check-label" for="etiquetaManualidades">Manualidades</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Deportes" id="etiquetaDeportes">
+                            <label class="form-check-label" for="etiquetaDeportes">Deportes</label>
+                        </div>
+                        <div class="form-chec form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Enseñanza" id="etiquetaEnsenanza">
+                            <label class="form-check-label" for="etiquetaEnsenanza">Enseñanza</label>
+                        </div>
+                        <div style="margin-top:5px;margin-bottom:5px;border-top:3px solid #454545"></div>
+                        <div class="form-check form-check-inline" style="margin-left:6px">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Tecnología" id="etiquetaTecnologia">
+                            <label class="form-check-label" for="etiquetaTecnologia">Tecnología</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Fotografía" id="etiquetaFotografia">
+                            <label class="form-check-label" for="etiquetaFotografia">Fotografía</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Jardinería" id="etiquetaJardineria">
+                            <label class="form-check-label" for="etiquetaJardineria">Jardinería</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Traducción" id="etiquetaTraduccion">
+                            <label class="form-check-label" for="etiquetaTraduccion">Traducción</label>
+                        </div>
+                        <div style="margin-top:5px;margin-bottom:5px;border-top:3px solid #454545"></div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Gestión de redes sociales" id="etiquetaGestionRedes">
+                            <label class="form-check-label" for="etiquetaGestionRedes">Gestión de redes sociales</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Escritura" id="etiquetaEscritura">
+                            <label class="form-check-label" for="etiquetaEscritura">Escritura</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Informática" id="etiquetaInformatica">
+                            <label class="form-check-label" for="etiquetaInformatica">Informática</label>
+                        </div>
+                        <div style="margin-top:5px;margin-bottom:5px;border-top:3px solid #454545"></div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Comunicación" id="etiquetaComunicacion">
+                            <label class="form-check-label" for="etiquetaComunicacion">Comunicación</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Contabilidad" id="etiquetaContabilidad">
+                            <label class="form-check-label" for="etiquetaContabilidad">Contabilidad</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Salud y bienestar" id="etiquetaSaludBienestar">
+                            <label class="form-check-label" for="etiquetaSaludBienestar">Salud y bienestar</label>
+                        </div>
+                        <div style="margin-top:5px;margin-bottom:5px;border-top:3px solid #454545"></div>
+                        <div class="form-check form-check-inline" style="margin-left:6px">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Investigación" id="etiquetaInvestigacion">
+                            <label class="form-check-label" for="etiquetaInvestigacion">Investigación</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Arte y creatividad" id="etiquetaArteCreatividad">
+                            <label class="form-check-label" for="etiquetaArteCreatividad">Arte y creatividad</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="checkbox" name="etiquetas[]" value="Entretenimiento" id="etiquetaEntretenimiento">
+                            <label class="form-check-label" for="etiquetaEntretenimiento">Entretenimiento</label>
+                        </div>
                     </div>
                 </div>
                 <div class="modal-footer">
